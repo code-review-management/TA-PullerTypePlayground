e7aefbd919587f39d1066ee1322fde8260678eab
nithinsenthil <nithin020304@gmail.com>
2026-01-30 03:40:50 -0800

    Add gh issue schema

--- prototype/lib/github.types.ts
+++ prototype/lib/github.types.ts
@@ -9,3 +9,18 @@ export type repo = {
     html_url: string
     description: string
 }
+
+export type issue = {
+    url: string
+    id: Number
+    number: Number
+    state: string
+    title: string
+    user: {
+        login: string
+        id: Number
+    }
+    body: string
+    created_at: string
+    updated_at: string
+}
