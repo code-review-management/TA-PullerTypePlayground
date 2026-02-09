368734aa1768c14063b349d9bc2575a05686433c
Valerie Doan <valeriephuongdoan@gmail.com>
2026-02-09 11:53:30 -0800

    delete: bare react-diff-view

--- prototype/app/(pages)/react-diff-view/page.tsx
+++ prototype/app/(pages)/react-diff-view/page.tsx
@@ -1,15 +1,15 @@
 "use client";
 
-import BareReactDiffView from "@/app/(components)/CodeRendering/ReactDiffView/BareReactDiffView";
 import { Container, Typography } from "@mui/material";
+import ReactDiffView from "@/app/(components)/CodeRendering/ReactDiffView/ReactDiffView";
 
 export default function ReactDiffViewPage() {
   return (
     <Container>
       <Typography component="h1" variant="h4" gutterBottom>
-        react-diff-view (out-of-box bare component with no modifications)
+        react-diff-view
       </Typography>
-      <BareReactDiffView />
+      <ReactDiffView />
     </Container>
   );
 }
