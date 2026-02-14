442bb84634f1e7ff70a35a321bc6f7d33ab9ea23
Valerie Doan <valeriephuongdoan@gmail.com>
2026-02-14 08:53:20 -0800

    chore: Add types for env variables

---
+++ code-review-management/types/environment.d.ts
@@ -0,0 +1,16 @@
+/**
+ * Docs:
+ * 1. https://stackoverflow.com/questions/69461972/typescript-says-nextjs-environment-variables-are-undefined
+ */
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    SUPABASE_URL: string;
+    SUPABASE_SERVICE_ROLE_KEY: string;
+    AUTH_GITHUB_ID: string;
+    AUTH_GITHUB_SECRET: string;
+    APP_ID: string;
+    WEBHOOK_SECRET: string;
+    PRIVATE_KEY_PATH: string;
+  }
+}
