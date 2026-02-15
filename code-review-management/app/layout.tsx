916cc653a59d668b8586a1e96102d770f323b3d0
Valerie Doan <valeriephuongdoan@gmail.com>
2026-02-15 15:13:16 -0800

    feat: Create global colors style

--- code-review-management/app/layout.tsx
+++ code-review-management/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import "@globals/styles/globals.css";
+import "@globals/styles/colors.css";
 import "@globals/styles/spacers.css";
 
 const roboto = Roboto({
