1f2b1c763cf129b9945bfa1895784836cbf3983a
Valerie Doan <valeriephuongdoan@gmail.com>
2026-02-17 21:46:40 -0800

    format: Remove whitespace in FileDiffView.tsx

--- code-review-management/app/(pages)/[username]/[repo_name]/pull/[id]/changes/_components/FileDiffView/FileDiffView.tsx
+++ code-review-management/app/(pages)/[username]/[repo_name]/pull/[id]/changes/_components/FileDiffView/FileDiffView.tsx
@@ -51,7 +51,6 @@ export default function FileDiffView({
   publishedThreadsByLine: PublishedThreadsByLine;
 }) {
   const [isExpanded, setIsExpanded] = useState(true);
-
   const tokens = tokenize(hunks, {
     highlight: true,
     refractor: refractor,
