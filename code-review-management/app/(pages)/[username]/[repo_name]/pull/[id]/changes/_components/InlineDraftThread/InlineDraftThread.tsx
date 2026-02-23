b15e81eeb753e5af48b2d93d8094e02c6dd7e231
Valerie Doan <valeriephuongdoan@gmail.com>
2026-02-22 16:03:01 -0800

    style: Make draft thread UI clearer

--- code-review-management/app/(pages)/[username]/[repo_name]/pull/[id]/changes/_components/InlineDraftThread/InlineDraftThread.tsx
+++ code-review-management/app/(pages)/[username]/[repo_name]/pull/[id]/changes/_components/InlineDraftThread/InlineDraftThread.tsx
@@ -1,10 +1,11 @@
 import { DraftThreadItem } from "../../_hooks/useDraftThreads";
+import styles from "./InlineDraftThread.module.css";
 
 /**
- * A draft thread anchored to specific lines in a file diff. Will contain an
- * editor where the user can draft a comment that will begin a new thread upon
- * submission.
- * 
+ * Component to display a draft thread that is anchored to specific lines in a
+ * file diff. Will eventually contain an editor where the user can draft a
+ * comment associated with those lines, and begin a new thread upon submission.
+ *
  * @param draft: `DraftThreadItem` object containing data about the draft thread.
  */
 export default function InlineDraftThread({
@@ -12,5 +13,32 @@ export default function InlineDraftThread({
 }: {
   draft: DraftThreadItem;
 }) {
-  return <div>Draft created at {draft.created}, {draft.start}, {draft.end} </div>;
+  return (
+    <div>
+      <div className={styles.header}>{getDraftHeader(draft)}</div>
+      <div>Created at {draft.created}</div>
+    </div>
+  );
+}
+
+/**
+ * Gets the header text for the draft thread. Indicates which sides and lines
+ * the draft is anchored to.
+ *
+ * TODO: Only indicate the side ("R" or "L") if it is a modified file.
+ * Otherwise, if it is a new file or deleted file, react-diff-view does not do a
+ * split view, so it does not make sense to show "R" or "L".
+ *
+ * @param draft `DraftThreadItem` object containing data about the draft thread.
+ */
+function getDraftHeader(draft: DraftThreadItem) {
+  const side = draft.side === "new" ? "R" : "L";
+  const end = `${side}${draft.end}`;
+
+  if (draft.start !== draft.end) {
+    const start = `${side}${draft.start}`;
+    return `Draft on lines ${start} to ${end}`;
+  }
+
+  return `Draft on line ${end}`;
 }
