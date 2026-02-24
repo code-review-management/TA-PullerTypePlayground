ac91004999f730c9d24254c86722df5bfe1e9658
Naomi Zhao <naomitan.zhao@gmail.com>
2026-02-24 14:26:36 -0800

    feat(pr-view): Add links to PRHeader buttons

--- code-review-management/app/(pages)/[username]/[repo_name]/pull/[id]/_components/PRHeader/PRHeader.tsx
+++ code-review-management/app/(pages)/[username]/[repo_name]/pull/[id]/_components/PRHeader/PRHeader.tsx
@@ -1,12 +1,16 @@
 import HeaderButton from "@/app/(pages)/_components/HeaderButton/HeaderButton";
 import styles from "./PRHeader.module.css";
 import { useEffect, useState } from "react";
+import { useParams } from "next/navigation";
 
 /**
  * Header for PR view page.
+ * TODO: Add callback as onClick to Merge button.
  */
 export default function PRHeader() {
   const [isScrolled, setIsScrolled] = useState<boolean>(false);
+  const params = useParams();
+  const { username, repo_name, id } = params;
 
   // On scroll, check the scrollY to determine whether to show box shadow or not.
   useEffect(() => {
@@ -17,7 +21,7 @@ export default function PRHeader() {
         setIsScrolled(false);
       }
     };
-    
+
     window.removeEventListener("scroll", onScroll);
     window.addEventListener("scroll", onScroll, { passive: true });
     return () => window.removeEventListener("scroll", onScroll);
@@ -25,8 +29,18 @@ export default function PRHeader() {
 
   return (
     <div className={`${styles.PRHeader} ${isScrolled && styles.shadow}`}>
-      <HeaderButton variant="secondary">View Files</HeaderButton>
-      <HeaderButton variant="secondary">Add review</HeaderButton>
+      <HeaderButton
+        href={`/${username}/${repo_name}/pull/${id}/changes`}
+        variant="secondary"
+      >
+        View Files
+      </HeaderButton>
+      <HeaderButton
+        href={`/${username}/${repo_name}/pull/${id}/changes`}
+        variant="secondary"
+      >
+        Add review
+      </HeaderButton>
       <HeaderButton>Merge</HeaderButton>
     </div>
   );
