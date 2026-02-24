8619a0d3cea3c2c073480a88de65506274e0127f
Naomi Zhao <naomitan.zhao@gmail.com>
2026-02-24 14:18:01 -0800

    chore(pr-view): Write comments for PR header components

--- code-review-management/app/(pages)/[username]/[repo_name]/pull/[id]/_components/PRHeader/PRHeader.tsx
+++ code-review-management/app/(pages)/[username]/[repo_name]/pull/[id]/_components/PRHeader/PRHeader.tsx
@@ -2,9 +2,13 @@ import HeaderButton from "@/app/(pages)/_components/HeaderButton/HeaderButton";
 import styles from "./PRHeader.module.css";
 import { useEffect, useState } from "react";
 
+/**
+ * Header for PR view page.
+ */
 export default function PRHeader() {
   const [isScrolled, setIsScrolled] = useState<boolean>(false);
 
+  // On scroll, check the scrollY to determine whether to show box shadow or not.
   useEffect(() => {
     const onScroll = () => {
       if (window.scrollY > 0) {
@@ -13,7 +17,7 @@ export default function PRHeader() {
         setIsScrolled(false);
       }
     };
-    // clean up code
+    
     window.removeEventListener("scroll", onScroll);
     window.addEventListener("scroll", onScroll, { passive: true });
     return () => window.removeEventListener("scroll", onScroll);
--- code-review-management/app/(pages)/_components/HeaderButton/HeaderButton.tsx
+++ code-review-management/app/(pages)/_components/HeaderButton/HeaderButton.tsx
@@ -2,6 +2,18 @@ import styles from "./HeaderButton.module.css";
 import Link from "next/link";
 import { MouseEventHandler, ReactNode } from "react";
 
+/**
+ * The button component used in the PR view / PR change page headers.
+ * Between onClick and href, only one param (at most) should be provided. Whichever parameter is
+ * provided determines the underlying HTML element implementing the header button.
+ * 
+ * @param onClick onClick function called when button is clicked. 
+ *               If this is provided (and href is not), the button will use <button> element.
+ * @param href The link to direct to when button is clicked.
+ *               If this is provided (and onClick is not), the button will use <Link> element.
+ * @param variant?: Which style variant of the button to use. Either "primary" or "secondary". defaults to primary
+ * @param children: Button inner content.
+ */
 export default function HeaderButton({
   onClick,
   href,
