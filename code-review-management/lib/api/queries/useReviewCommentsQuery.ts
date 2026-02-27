4caae17f86d10ac09af3ab89a30c2fa257a1374b
Valerie Doan <valeriephuongdoan@gmail.com>
2026-02-26 21:04:14 -0800

    feat(tanstack): Add hook for fetching review comments

---
+++ code-review-management/lib/api/queries/useReviewCommentsQuery.ts
@@ -0,0 +1,23 @@
+import { useQuery } from "@tanstack/react-query";
+import { fetcher } from "../utils/fetcher";
+import { Comment } from "@/types/github.types";
+
+/**
+ * Fetches the review comments on the diffs of a GitHub pull request.
+ *
+ * @param owner: Owner of the repository.
+ * @param repo: Name of the repository.
+ * @param pullNumber: Pull request number.
+ * @returns: TanStack query result containing the pull request data.
+ */
+export function useReviewCommentsQuery(
+  owner: string,
+  repo: string,
+  pullNumber: string,
+) {
+  return useQuery({
+    queryKey: ["review-comments", owner, repo, pullNumber],
+    queryFn: async (): Promise<Comment> =>
+      fetcher(`/api/v1/${owner}/${repo}/pulls/${pullNumber}/comments`),
+  });
+}
