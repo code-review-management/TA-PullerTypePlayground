06c9e427390fbd007eced3e756177339f4fb981c
Valerie Doan <valeriephuongdoan@gmail.com>
2026-02-28 21:58:25 -0800

    docs: Comment out FileTree

--- code-review-management/app/(pages)/[username]/[repo_name]/pull/[id]/changes/_components/FileTree/FileTree.tsx
+++ code-review-management/app/(pages)/[username]/[repo_name]/pull/[id]/changes/_components/FileTree/FileTree.tsx
@@ -1,14 +1,19 @@
 import { useMemo } from "react";
 import { FileDiff } from "@/types/github.types";
-import { buildFileTree } from "../../_utils/filetree-utils";
+import { buildFileTree, FileTreeNode } from "../../_utils/filetree-utils";
 import FileTreeRow from "../FileTreeRow/FileTreeRow";
 import styles from "./FileTree.module.css";
 
+/**
+ * Tree of the pull request files.
+ * 
+ * @param files: List of `FileDiff` objects for each of the pull request files.
+ */
 export default function FileTree({ files }: { files: FileDiff[] }) {
   const fileTree = useMemo(() => buildFileTree(files), [files]);
   return (
     <div className={styles.tree}>
-      {fileTree.map((node) => (
+      {fileTree.map((node: FileTreeNode) => (
         <FileTreeRow key={node.name} node={node} />
       ))}
     </div>
