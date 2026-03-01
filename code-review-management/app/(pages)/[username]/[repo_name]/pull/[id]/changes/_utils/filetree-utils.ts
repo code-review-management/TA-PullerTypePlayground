7dbb072e092967263cf71f59ed5286d4986ad74f
Valerie Doan <valeriephuongdoan@gmail.com>
2026-02-28 22:34:14 -0800

    docs: Comment out filetree-utils

--- code-review-management/app/(pages)/[username]/[repo_name]/pull/[id]/changes/_utils/filetree-utils.ts
+++ code-review-management/app/(pages)/[username]/[repo_name]/pull/[id]/changes/_utils/filetree-utils.ts
@@ -7,24 +7,24 @@ export type FileTreeNode =
   | { type: "file"; name: string; fileDiff: FileDiff };
 
 class PrefixTrieNode {
-  public children: Map<Segment, PrefixTrieNode>;
-  public isFile: boolean;
-  public fileDiff?: FileDiff;
+  children: Map<Segment, PrefixTrieNode>;
+  isFile: boolean;
+  fileDiff?: FileDiff; // `FileDiff` objects included if `isFile` is true.
 
-  public constructor() {
+  constructor() {
     this.children = new Map();
     this.isFile = false;
   }
 }
 
 class PrefixTrie {
-  public root: PrefixTrieNode;
+  root: PrefixTrieNode;
 
-  public constructor() {
+  constructor() {
     this.root = new PrefixTrieNode();
   }
 
-  public insert(fileDiff: FileDiff) {
+  insert(fileDiff: FileDiff) {
     const segments = fileDiff.filename.split("/");
 
     let node = this.root;
@@ -35,14 +35,16 @@ class PrefixTrie {
       node = node.children.get(segment)!;
     }
 
-    node.isFile = true;
-    node.fileDiff = fileDiff;
+    node.isFile = true; // Set `isFile` to true for leaf nodes.
+    node.fileDiff = fileDiff; // Include `FileDiff` object for leaf nodes.
   }
 
-  public traverse(parent: PrefixTrieNode) {
+  traverse(parent: PrefixTrieNode) {
+    // Assemble an array of directories and files at the same level in the tree.
     const directories: FileTreeNode[] = [];
     const files: FileTreeNode[] = [];
 
+    // Traverse through all the children directly connected to the current node.
     for (const [segment, child] of parent.children) {
       if (child.isFile) {
         files.push({ type: "file", name: segment, fileDiff: child.fileDiff! });
@@ -50,7 +52,10 @@ class PrefixTrie {
         let collapsedName = segment;
         let node = child;
 
+        // Collapse directory names if the current directory contains only
+        // 1 child that is also a directory.
         while (node.children.size === 1) {
+          // Docs: https://stackoverflow.com/a/32373389 (gets the first item in a map)
           const next = node.children.entries().next().value!;
           const [nextSegment, nextChild] = next;
 
@@ -68,13 +73,21 @@ class PrefixTrie {
       }
     }
 
+    // Directories and files at the same level are sorted lexicographically.
     directories.sort((a, b) => a.name.localeCompare(b.name));
     files.sort((a, b) => a.name.localeCompare(b.name));
 
+    // Directories should precede files when they are at the same level.
     return [...directories, ...files];
   }
 }
 
+/**
+ * Builds a prefix trie from each file's name, then traverses it to produce a
+ * data structure that resembles their file path hierarchy.
+ * 
+ * @param fileDiffs: List of `FileDiff` objects for each of the pull request files.
+ */
 export function buildFileTree(fileDiffs: FileDiff[]) {
   const trie = new PrefixTrie();
   fileDiffs.forEach((fileDiff) => trie.insert(fileDiff));
