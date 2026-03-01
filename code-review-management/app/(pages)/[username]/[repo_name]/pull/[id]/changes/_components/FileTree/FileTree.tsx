52cbc8e423c1c3e0aeded96c63635e9a4c60bdef
Valerie Doan <valeriephuongdoan@gmail.com>
2026-02-28 16:53:22 -0800

    feat(diff-view): Display simple file tree

--- code-review-management/app/(pages)/[username]/[repo_name]/pull/[id]/changes/_components/FileTree/FileTree.tsx
+++ code-review-management/app/(pages)/[username]/[repo_name]/pull/[id]/changes/_components/FileTree/FileTree.tsx
@@ -1,16 +1,47 @@
+import { useMemo, useState } from "react";
 import { FileDiff } from "@/types/github.types";
-import { buildFileTree } from "../../_utils/filetree-utils";
+import { buildFileTree, FileTreeNode } from "../../_utils/filetree-utils";
 import styles from "./FileTree.module.css";
 
 export default function FileTree({ files }: { files: FileDiff[] }) {
-  buildFileTree(files);
-
+  const fileTree = useMemo(() => buildFileTree(files), [files]);
   return (
     <div className={styles.tree}>
-      {/* https://stackoverflow.com/a/66389669 */}
-      {Array.from({ length: 100 }).map((_, index) => (
-        <div key={index}>Test</div>
+      {fileTree.map((node) => (
+        <FileTreeRow key={node.name} node={node} />
       ))}
     </div>
+  );
+}
+
+function FileTreeRow({
+  node,
+  depth = 0,
+}: {
+  node: FileTreeNode;
+  depth?: number;
+}) {
+  const [isExpanded, setIsExpanded] = useState(true);
+  const xPadding = depth * 16 + 8;
+
+  return (
+    <>
+      <div
+        className={styles.row}
+        style={{ paddingLeft: xPadding, paddingRight: xPadding }}
+        onClick={
+          node.type === "directory"
+            ? () => setIsExpanded((prev) => !prev)
+            : undefined
+        }
+      >
+        {node.name}
+      </div>
+      {node.type === "directory" &&
+        isExpanded &&
+        node.children.map((child) => (
+          <FileTreeRow key={child.name} node={child} depth={depth + 1} />
+        ))}
+    </>
   );
 }
