64002548de80254c498c309fec0aeeddf2ed56a2
Valerie Doan <valeriephuongdoan@gmail.com>
2026-02-28 16:35:13 -0800

    feat(diff-view): Traverse prefix nodes to get file tree

--- code-review-management/app/(pages)/[username]/[repo_name]/pull/[id]/changes/_utils/filetree-utils.ts
+++ code-review-management/app/(pages)/[username]/[repo_name]/pull/[id]/changes/_utils/filetree-utils.ts
@@ -2,13 +2,17 @@ import { FileDiff } from "@/types/github.types";
 
 type Segment = string;
 
+export type FileTreeNode =
+  | { type: "directory"; name: string; children: FileTreeNode[] }
+  | { type: "file"; name: string; fileDiff: FileDiff };
+
 class PrefixTrieNode {
-  public children: Record<Segment, PrefixTrieNode>;
+  public children: Map<Segment, PrefixTrieNode>;
   public isFile: boolean;
-  public file?: FileDiff;
+  public fileDiff?: FileDiff;
 
   public constructor() {
-    this.children = {};
+    this.children = new Map();
     this.isFile = false;
   }
 }
@@ -20,22 +24,59 @@ class PrefixTrie {
     this.root = new PrefixTrieNode();
   }
 
-  public insert(file: FileDiff) {
-    const segments = file.filename.split("/");
+  public insert(fileDiff: FileDiff) {
+    const segments = fileDiff.filename.split("/");
+
     let node = this.root;
     for (const segment of segments) {
-      if (!(segment in node.children)) {
-        node.children[segment] = new PrefixTrieNode();
+      if (!node.children.has(segment)) {
+        node.children.set(segment, new PrefixTrieNode());
       }
-      node = node.children[segment];
+      node = node.children.get(segment)!;
     }
+
     node.isFile = true;
-    node.file = file;
+    node.fileDiff = fileDiff;
+  }
+
+  public traverse(parent: PrefixTrieNode) {
+    const directories: FileTreeNode[] = [];
+    const files: FileTreeNode[] = [];
+
+    for (const [segment, child] of parent.children) {
+      if (child.isFile) {
+        files.push({ type: "file", name: segment, fileDiff: child.fileDiff! });
+      } else {
+        let collapsedName = segment;
+        let node = child;
+
+        while (node.children.size === 1) {
+          const next = node.children.entries().next().value!;
+          const [nextSegment, nextChild] = next;
+
+          if (nextChild.isFile) break;
+
+          collapsedName += nextSegment;
+          node = nextChild;
+        }
+
+        directories.push({
+          type: "directory",
+          name: collapsedName,
+          children: this.traverse(node),
+        });
+      }
+    }
+
+    directories.sort((a, b) => a.name.localeCompare(b.name));
+    files.sort((a, b) => a.name.localeCompare(b.name));
+
+    return [...directories, ...files];
   }
 }
 
-export function buildFileTree(files: FileDiff[]) {
-  const fileTree = new PrefixTrie();
-  files.forEach((file) => fileTree.insert(file));
-  console.log(fileTree);
+export function buildFileTree(fileDiffs: FileDiff[]) {
+  const trie = new PrefixTrie();
+  fileDiffs.forEach((fileDiff) => trie.insert(fileDiff));
+  return trie.traverse(trie.root);
 }
