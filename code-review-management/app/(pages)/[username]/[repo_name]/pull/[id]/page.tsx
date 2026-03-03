e0a344a263280ad16c44bb8f4bd7199755a9759b
Naomi Zhao <naomitan.zhao@gmail.com>
2026-03-03 14:24:28 -0800

    fix(pr-view): Resolve compiler warnings

--- code-review-management/app/(pages)/[username]/[repo_name]/pull/[id]/page.tsx
+++ code-review-management/app/(pages)/[username]/[repo_name]/pull/[id]/page.tsx
@@ -3,7 +3,6 @@ import Divider from "@/app/(pages)/_components/Divider/Divider";
 import PullBodyDescription from "./_components/PullBodyDescription/PullBodyDescription";
 import PullBodyHeader from "./_components/PullBodyHeader/PullBodyHeader";
 import styles from "./page.module.css";
-import MOCK_PULL from "@/mocks/pull.json";
 import StatusSection from "./_components/StatusSection/StatusSection";
 import Reviewers from "./_components/Reviewers/Reviewers";
 import Assignees from "./_components/Assignees/Assignees";
@@ -13,7 +12,6 @@ import { useParams } from "next/navigation";
 import { PullParams } from "@/types/routing.types";
 import { usePullQuery } from "@/lib/api/queries/usePullQuery";
 import PRViewTimeline from "./_components/PRViewTimeline/PRViewTimeline";
-// import { useParams } from 'next/navigation'
 
 // Pull Request View page.
 export default function Pull() {
@@ -36,18 +34,18 @@ export default function Pull() {
           <PullBodyHeader pullData={data} />
           <Divider />
           <PullBodyDescription
-            username={data.user?.login}
+            username={data.user?.login || ""}
             createdAt={data.created_at}
-            description={data.body}
+            description={data.body || ""}
           />
           <PRViewTimeline />
         </div>
         <div className={styles.infoColumn}>
           <StatusSection pullData={data} />
           <Divider />
-          <Reviewers reviewers={data.requested_reviewers} />
+          <Reviewers reviewers={data.requested_reviewers || []} />
           <Divider />
-          <Assignees assignees={data.assignees}/>
+          <Assignees assignees={data.assignees || []}/>
           <Divider />
           <CISection />
           <Divider />
