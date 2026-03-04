3c8699ff68b0ed2b43265202cb170209bb8733b8
Naomi Zhao <naomitan.zhao@gmail.com>
2026-03-04 13:47:46 -0800

    chore(pr-view): Remove unused variables from Assigneees and Reviewers components

--- code-review-management/app/(pages)/[username]/[repo_name]/pull/[id]/_components/Assignees/Assignees.tsx
+++ code-review-management/app/(pages)/[username]/[repo_name]/pull/[id]/_components/Assignees/Assignees.tsx
@@ -4,14 +4,6 @@ import UserLister from "../UserLister/UserLister";
 /**
  * Assignees section of the PR view page.
  */
-export default function Assignees({assignees}: {assignees:User[]}) {
-  // TODO: Pass in props for listed users and to edit listed users.
-  const listedUsers = [
-    {
-      username: "octocat",
-      imageSrc: "/mock/octocat.png",
-    },
-  ];
-
+export default function Assignees({ assignees }: { assignees: User[] }) {
   return <UserLister listType={"assignees"} userList={assignees} />;
 }
--- code-review-management/app/(pages)/[username]/[repo_name]/pull/[id]/_components/Reviewers/Reviewers.tsx
+++ code-review-management/app/(pages)/[username]/[repo_name]/pull/[id]/_components/Reviewers/Reviewers.tsx
@@ -4,14 +4,6 @@ import UserLister from "../UserLister/UserLister";
 /**
  * Reviewers section of the PR view page.
  */
-export default function Reviewers({reviewers} : {reviewers: User[]}) {
-  // TODO: Pass in props for listed users and to edit listed users.
-  const listedUsers = [
-    {
-      username: "octodog",
-      imageSrc: "/mock/octodog.png",
-    },
-  ];
-
+export default function Reviewers({ reviewers }: { reviewers: User[] }) {
   return <UserLister listType={"reviewers"} userList={reviewers} />;
 }
