bec4ec6f75518726a58d1bbca2bd9ee1492c6587
Naomi Zhao <naomitan.zhao@gmail.com>
2026-03-04 00:27:48 -0800

    chore(pr-view): Write UserIcon comments

--- code-review-management/app/(pages)/_components/UserIcon/UserIcon.tsx
+++ code-review-management/app/(pages)/_components/UserIcon/UserIcon.tsx
@@ -1,6 +1,13 @@
 import Image from "next/image";
 import styles from "./UserIcon.module.css";
 
+/**
+ * User icon image with given src, username for alt text, and size (width and height).
+ * @param avatarUrl Image source URL, generally provided by API responses
+ * @param username GitHub username of the user this icon is for, used to create alt text
+ * @param size Used for height and width of the icon
+ * @returns 
+ */
 export default function UserIcon({
   avatarUrl,
   username,
