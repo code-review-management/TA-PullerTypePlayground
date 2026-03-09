8634041e5bd9bf37d663eba4de48dcc7bb8a0d64
Valerie Doan <valeriephuongdoan@gmail.com>
2026-03-09 15:23:57 -0700

    feat(add-review): Add optional onClick handler for submit button

--- code-review-management/app/(pages)/_components/SubmitButton/SubmitButton.tsx
+++ code-review-management/app/(pages)/_components/SubmitButton/SubmitButton.tsx
@@ -1,22 +1,27 @@
+import { MouseEventHandler } from "react";
 import styles from "./SubmitButton.module.css";
 
 /**
- * A button used for submission.
+ * A button used for submissions.
  *
- * @param label: Label to display on the button
+ * @param label: Label to display on the button.
  * @param isLabeled: Whether the button is disabled or not.
+ * @param onClick: Function called when button is clicked. 
  */
 export default function SubmitButton({
   label,
   isDisabled,
+  onClick,
 }: {
   label: string;
   isDisabled: boolean;
+  onClick?: MouseEventHandler<HTMLButtonElement>;
 }) {
   return (
     <button
       className={`${styles.submit} ${isDisabled ? styles.disabled : ""}`}
       disabled={isDisabled}
+      onClick={onClick ?? undefined}
     >
       {label}
     </button>
