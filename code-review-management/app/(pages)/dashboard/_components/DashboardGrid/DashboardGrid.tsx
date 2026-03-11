e2ecb901a62ebda1bf80e992ba789899409be9bd
Naomi Zhao <naomitan.zhao@gmail.com>
2026-03-11 16:02:30 -0700

    init(dashboard): Add Dashboard mock data and start ui

---
+++ code-review-management/app/(pages)/dashboard/_components/DashboardGrid/DashboardGrid.tsx
@@ -0,0 +1,5 @@
+import styles from "./DashboardGrid.module.css"
+
+export default function DashboardGrid() {
+    return <div className={styles.dashboardGrid}>dashboard grid</div>
+}
--- code-review-management/app/(pages)/dashboard/page.tsx
+++ code-review-management/app/(pages)/dashboard/page.tsx
@@ -1,9 +1,11 @@
-import styles from "./page.module.css"
+import DashboardGrid from "./_components/DashboardGrid/DashboardGrid";
+import styles from "./page.module.css";
 
 export default function Dashboard() {
-    return(
-        <div className={styles.page}>
-            Dashboard
-        </div>
-    );
+  return (
+    <div className={styles.page}>
+      <div className={styles.repoSideBar} />
+      <DashboardGrid />
+    </div>
+  );
 }
