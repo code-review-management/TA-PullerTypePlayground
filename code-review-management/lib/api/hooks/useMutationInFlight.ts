b2fcbda0a28fe07e2e84e83d8bd2c72240500e5e
Valerie Doan <valeriephuongdoan@gmail.com>
2026-03-11 20:07:25 -0700

    feat(tanstack): Create hook for checking if a mutation is in flight

---
+++ code-review-management/lib/api/hooks/useMutationInFlight.ts
@@ -0,0 +1,25 @@
+import { MutationKey, useMutationState } from "@tanstack/react-query";
+
+/**
+ * Check if there exists a mutation with the given key that is currently in-
+ * flight.
+ *
+ * Docs:
+ * 1. https://tanstack.com/query/v5/docs/framework/react/reference/useMutationState
+ *
+ * @param mutationKey: Unique key to identify the mutation.
+ */
+export function useMutationInFlight({
+  mutationKey,
+}: {
+  mutationKey: MutationKey;
+}) {
+  return (
+    useMutationState({
+      filters: {
+        mutationKey,
+        status: "pending",
+      },
+    }).length > 0
+  );
+}
