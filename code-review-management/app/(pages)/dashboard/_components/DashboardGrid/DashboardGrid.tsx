e3f80e401f5bdf3fda12e62ef5f49127b75af7cd
Naomi Zhao <naomitan.zhao@gmail.com>
2026-03-11 17:31:17 -0700

    feat(dashboard): Add more information and styling

--- code-review-management/app/(pages)/dashboard/_components/DashboardGrid/DashboardGrid.tsx
+++ code-review-management/app/(pages)/dashboard/_components/DashboardGrid/DashboardGrid.tsx
@@ -1,3 +1,4 @@
+import UserIcon from "@/app/(pages)/_components/UserIcon/UserIcon";
 import styles from "./DashboardGrid.module.css";
 import MOCK_PULLS from "@/mocks/dashboard_pulls.json";
 
@@ -5,26 +6,34 @@ export default function DashboardGrid() {
   return (
     <table className={styles.dashboardGrid}>
       <thead>
-          <tr className={styles.gridHeader}>
-            <th className={styles.iconWidth} />
-            <th className={styles.titleWidth}>Title</th>
-            <th className={styles.assigneesWidth}>Assignees</th>
-            <th className={styles.reviewersWidth}>Reviewers</th>
-            <th className={styles.statusWidth}>Status</th>
-            <th className={styles.updatedWidth}>Updated</th>
-          </tr>
+        <tr className={styles.gridHeader}>
+          <th className={styles.iconWidth} />
+          <th className={styles.titleWidth}>Title</th>
+          <th className={styles.assigneesWidth}>Assignees</th>
+          <th className={styles.reviewersWidth}>Reviewers</th>
+          <th className={styles.statusWidth}>Status</th>
+          <th className={styles.updatedWidth}>Updated</th>
+        </tr>
       </thead>
-      <tbody>
-          {MOCK_PULLS.map((pull) => (
-            <tr key={pull.id} className={styles.gridRow}>
-              <td className={styles.iconWidth}></td>
-              <td className={styles.titleWidth}>{pull.title}</td>
-              <td className={styles.assigneesWidth}></td>
-              <td className={styles.reviewersWidth}></td>
-              <td className={styles.statusWidth}></td>
-              <td className={styles.updatedWidth}></td>
-            </tr>
-          ))}
+      <tbody className={styles.gridBody}>
+        {MOCK_PULLS.map((pull) => (
+          <tr key={pull.id} className={styles.gridRow}>
+            <td className={styles.iconWidth}><UserIcon avatarUrl={pull.user.avatar_url} username={pull.user.login} size={40}/></td>
+            <td className={`${styles.rowTitle} ${styles.titleWidth}`}>
+              <div className={styles.rowTitleTop}>
+                <h4 className={styles.titleTitle}>{pull.title}</h4>
+                <span className={styles.titleNumber}>#{pull.number}</span>
+              </div>
+              <span className={styles.rowTitleBottom}>
+                {pull.head.repo.name}
+              </span>
+            </td>
+            <td className={styles.assigneesWidth}></td>
+            <td className={styles.reviewersWidth}></td>
+            <td className={styles.statusWidth}></td>
+            <td className={styles.updatedWidth}></td>
+          </tr>
+        ))}
       </tbody>
     </table>
   );
