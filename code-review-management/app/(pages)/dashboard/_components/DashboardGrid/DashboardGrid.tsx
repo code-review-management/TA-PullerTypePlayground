e710d1e743e539e7c50c10619f49de829030b50a
Naomi Zhao <naomitan.zhao@gmail.com>
2026-03-11 17:55:24 -0700

    feat(dashboard): Add stacked user icon list to rows

--- code-review-management/app/(pages)/dashboard/_components/DashboardGrid/DashboardGrid.tsx
+++ code-review-management/app/(pages)/dashboard/_components/DashboardGrid/DashboardGrid.tsx
@@ -1,6 +1,7 @@
 import UserIcon from "@/app/(pages)/_components/UserIcon/UserIcon";
 import styles from "./DashboardGrid.module.css";
 import MOCK_PULLS from "@/mocks/dashboard_pulls.json";
+import { User } from "@/types/github.types";
 
 export default function DashboardGrid() {
   return (
@@ -18,7 +19,13 @@ export default function DashboardGrid() {
       <tbody className={styles.gridBody}>
         {MOCK_PULLS.map((pull) => (
           <tr key={pull.id} className={styles.gridRow}>
-            <td className={styles.iconWidth}><UserIcon avatarUrl={pull.user.avatar_url} username={pull.user.login} size={40}/></td>
+            <td className={styles.iconWidth}>
+              <UserIcon
+                avatarUrl={pull.user.avatar_url}
+                username={pull.user.login}
+                size={40}
+              />
+            </td>
             <td className={`${styles.rowTitle} ${styles.titleWidth}`}>
               <div className={styles.rowTitleTop}>
                 <h4 className={styles.titleTitle}>{pull.title}</h4>
@@ -28,13 +35,37 @@ export default function DashboardGrid() {
                 {pull.head.repo.name}
               </span>
             </td>
-            <td className={styles.assigneesWidth}></td>
-            <td className={styles.reviewersWidth}></td>
+            <td className={styles.assigneesWidth}>
+              <UserIconList users={pull.assignees} />
+            </td>
+            <td className={styles.reviewersWidth}>
+              <UserIconList users={pull.requested_reviewers} />
+            </td>
             <td className={styles.statusWidth}></td>
             <td className={styles.updatedWidth}></td>
           </tr>
         ))}
       </tbody>
     </table>
+  );
+}
+
+function UserIconList({ users }: { users: User[] }) {
+  return (
+    <div className={styles.userIconList}>
+      {users.map((reviewer, idx) => (
+        <div
+          key={reviewer.login}
+          className={styles.userIconListItem}
+          style={{ zIndex: -idx }}
+        >
+          <UserIcon
+            avatarUrl={reviewer.avatar_url}
+            username={reviewer.login}
+            size={32}
+          />
+        </div>
+      ))}
+    </div>
   );
 }
