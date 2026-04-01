bb8775845fa7c745481373dfa8f9c04f5711ca14
Valerie Doan <valeriephuongdoan@gmail.com>
2026-04-01 12:48:58 -0700

    docs(auth): Include link to Auth.js docs for page protection

--- code-review-management/proxy.ts
+++ code-review-management/proxy.ts
@@ -1,5 +1,10 @@
 export { auth as proxy } from "@/lib/auth";
 
+/**
+ * Docs:
+ * 1. https://authjs.dev/getting-started/session-management/protecting?framework=Next.js
+ * Referenced Next.js Proxy section to protect a set of pages using a proxy file.
+ */
 export const config = {
   matcher: ["/dashboard", "/:username/:repo_name/pull/:id*"],
 };
