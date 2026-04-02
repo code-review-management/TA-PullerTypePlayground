fcf8d5d4d1148940b73457420befcf6900774888
Nithin Senthil <nithin020304@gmail.com>
2026-04-02 09:43:46 -0700

    feat: add v2 wrapper type for repos

---
+++ code-review-management/types/github.types.v2.ts
@@ -0,0 +1,12 @@
+import { RepoSchema } from "@/types/github.types";
+import * as z from "zod";
+
+export type RepoV2 = z.infer<typeof RepoSchemaV2>;
+
+export const RepoSchemaV2 = z.object({
+  repos: z.array(RepoSchema),
+  prev: z.number().optional(),
+  next: z.number().optional(),
+  first: z.number(),
+  last: z.number().optional(),
+});
