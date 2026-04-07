943dc23d26e1a76fea33192a4f1470ed249ea652
Nithin Senthil <nithin020304@gmail.com>
2026-04-07 09:52:53 -0700

    feat: Add CommentV2 wrapper data type

--- code-review-management/types/github.types.wrapper.ts
+++ code-review-management/types/github.types.wrapper.ts
@@ -1,8 +1,13 @@
-import { PullRequestSchema, RepoSchema } from "@/types/github.types";
+import {
+  CommentSchema,
+  PullRequestSchema,
+  RepoSchema,
+} from "@/types/github.types";
 import * as z from "zod";
 
 export type RepoV2 = z.infer<typeof RepoSchemaV2>;
 export type PullRequestV2 = z.infer<typeof PullRequestSchemaV2>;
+export type CommentV2 = z.infer<typeof CommentSchemaV2>;
 
 export const RepoSchemaV2 = z.object({
   data: z.array(RepoSchema),
@@ -19,4 +24,12 @@ export const PullRequestSchemaV2 = z.object({
   first: z.number().optional(),
   last: z.number().optional(),
   totalCount: z.number(),
-})
+});
+
+export const CommentSchemaV2 = z.object({
+  data: z.array(CommentSchema),
+  prev: z.number().optional(),
+  next: z.number().optional(),
+  first: z.number().optional(),
+  last: z.number().optional(),
+});
