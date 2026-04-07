5b912073c5813bbcf75daabd7249337d0e1149f7
Valerie Doan <valeriephuongdoan@gmail.com>
2026-04-07 11:28:43 -0700

    feat(panel): Add empty message if no comments exist

--- code-review-management/app/(pages)/[username]/[repo_name]/pull/[id]/changes/_components/ActivityPanel/ActivityPanel.tsx
+++ code-review-management/app/(pages)/[username]/[repo_name]/pull/[id]/changes/_components/ActivityPanel/ActivityPanel.tsx
@@ -1,10 +1,8 @@
 import { PublishedThreads } from "../../_hooks/usePublishedThreads";
+import Image from "next/image";
 import InlinePublishedThread from "../InlinePublishedThread/InlinePublishedThread";
 import styles from "./ActivityPanel.module.css";
 
-/**
- * TODO: Message if comment list is empty.
- */
 export default function ActivityPanel({
   publishedThreads,
   isOpen,
@@ -25,18 +23,30 @@ export default function ActivityPanel({
           <div className={styles.tab}>Comments</div>
           <div className={styles.tab}>Timeline</div>
         </div>
-        <div className={styles.threads}>
-          {allThreads.map((thread) => {
-            return (
-              <div
-                key={`${thread.path}-${thread.id}`}
-                className={styles.thread}
-              >
-                <InlinePublishedThread thread={thread} viewType="panel" />
-              </div>
-            );
-          })}
-        </div>
+        {allThreads.length === 0 ? (
+          <div className={styles.emptyCommentsMessage}>
+            <Image
+              src="/icons/comment_discussion.svg"
+              alt="Comment discussion"
+              height={24}
+              width={24}
+            />
+            No comments on files yet.
+          </div>
+        ) : (
+          <div className={styles.threads}>
+            {allThreads.map((thread) => {
+              return (
+                <div
+                  key={`${thread.path}-${thread.id}`}
+                  className={styles.thread}
+                >
+                  <InlinePublishedThread thread={thread} viewType="panel" />
+                </div>
+              );
+            })}
+          </div>
+        )}
       </div>
     </div>
   );
