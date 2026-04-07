83dba4b61500b887223d6ae507f40c760396e559
Naomi Zhao <naomitan.zhao@gmail.com>
2026-04-07 12:21:01 -0700

    feat: Create usePullsQuery.ts

---
+++ code-review-management/lib/api/queries/usePullsQuery.ts
@@ -0,0 +1,27 @@
+import {
+  keepPreviousData,
+  useInfiniteQuery,
+  useQuery,
+} from "@tanstack/react-query";
+import { fetcher } from "@/lib/api/utils/fetcher";
+// import { PullRequest } from "@/types/github.types";
+import { PullRequestV2 } from "@/types/github.types.wrapper";
+
+/**
+ * Fetches a GitHub pull request.
+ *
+ * @param owner: Owner of the repository.
+ * @param repo: Name of the repository.
+ * @param pullNumber: Pull request number.
+ * @returns: TanStack query result containing the pull request data.
+ */
+export function usePullsQuery() {
+  return useInfiniteQuery({
+    queryKey: ["pulls"],
+    queryFn: ({ pageParam }): Promise<PullRequestV2> =>
+      fetcher(`/api/v2/pulls?page=${pageParam}`),
+    initialPageParam: 1,
+    getNextPageParam: (lastPage) => lastPage.next,
+    getPreviousPageParam: (firstPage) => firstPage.prev,
+  });
+}
