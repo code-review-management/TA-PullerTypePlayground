767a16ad368db5f735ad34f283ac6212e2b515ea
Valerie Doan <valeriephuongdoan@gmail.com>
2026-04-10 12:31:09 -0700

    feat(diff-view)(commits): Add selected commit state

--- code-review-management/app/(pages)/[username]/[repo_name]/pull/[id]/changes/_components/CommitPicker/CommitPicker.tsx
+++ code-review-management/app/(pages)/[username]/[repo_name]/pull/[id]/changes/_components/CommitPicker/CommitPicker.tsx
@@ -1,9 +1,14 @@
+import { Dispatch, SetStateAction, useState } from "react";
 import { formatDate } from "../../../_utils/date-utils";
 import MOCK_COMMITS from "@/mocks/commits.json";
 import PopoverContent from "@components/PopoverContent/PopoverContent";
 import styles from "./CommitPicker.module.css";
 
+const ALL_CHANGES = "all-changes";
+
 export default function CommitPicker() {
+  const [selected, setSelected] = useState(ALL_CHANGES);
+
   return (
     <PopoverContent>
       <div className={styles.container}>
@@ -12,12 +17,20 @@ export default function CommitPicker() {
         </p>
         <div>
           <label className={styles.option}>
-            <CommitPickerRadio />
+            <CommitRadio
+              value={ALL_CHANGES}
+              checked={selected === ALL_CHANGES}
+              onChange={setSelected}
+            />
             <span className={styles.message}>Show all changes</span>
           </label>
           {MOCK_COMMITS.map((commit) => (
             <label key={commit.sha} className={styles.option}>
-              <CommitPickerRadio />
+              <CommitRadio
+                value={commit.sha}
+                checked={selected === commit.sha}
+                onChange={setSelected}
+              />
               <span className={styles.info}>
                 <span className={styles.message}>{commit.commit.message}</span>
                 <span className={styles.meta}>
@@ -34,6 +47,24 @@ export default function CommitPicker() {
   );
 }
 
-function CommitPickerRadio() {
-  return <input type="radio" name="commit" required className={styles.radio} />;
+function CommitRadio({
+  value,
+  checked,
+  onChange,
+}: {
+  value: string;
+  checked: boolean;
+  onChange: Dispatch<SetStateAction<string>>;
+}) {
+  return (
+    <input
+      type="radio"
+      name="commit"
+      value={value}
+      required
+      checked={checked}
+      onChange={() => onChange(value)}
+      className={styles.radio}
+    />
+  );
 }
