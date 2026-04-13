060239e1e7fdc95427c7ccda23fbd372a0087c37
Naomi Zhao <naomitan.zhao@gmail.com>
2026-04-13 14:55:09 -0700

    Modify usePullsQuery comments and query usage

--- code-review-management/lib/api/queries/usePullsQuery.ts
+++ code-review-management/lib/api/queries/usePullsQuery.ts
@@ -1,30 +1,20 @@
-import {
-  InfiniteData,
-  keepPreviousData,
-  useInfiniteQuery,
-  useQuery,
-} from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import { fetcher } from "@/lib/api/utils/fetcher";
-// import { PullRequest } from "@/types/github.types";
 import { PullRequestV2 } from "@/types/github.types.wrapper";
-import { useCallback } from "react";
-import { PullRequest } from "@/types/github.types";
 
 /**
- * Fetches a GitHub pull request.
+ * Fetches list of pull requests relevant to the requesting user.
  *
- * @param owner: Owner of the repository.
- * @param repo: Name of the repository.
- * @param pullNumber: Pull request number.
+ * @param queryType:
  * @returns: TanStack query result containing the pull request data.
  */
-export function usePullsQuery<T>(
-  queryType?: "open" | "draft" | "merged",
-) {
+export function usePullsQuery<T>(queryType?: "open" | "draft" | "merged") {
+  const queryTypeString = queryType ? `&${queryType}=true` : "";
+
   return useInfiniteQuery({
     queryKey: ["pulls"],
     queryFn: async ({ pageParam }): Promise<PullRequestV2> =>
-      fetcher(`/api/v2/pulls?page=${pageParam}`),
+      fetcher(`/api/v2/pulls?page=${pageParam}${queryTypeString}`),
     initialPageParam: 1,
     getNextPageParam: (lastPage) => lastPage.next,
     getPreviousPageParam: (firstPage) => firstPage.prev,
