04fbf8c08737024a22de21464c0ff0a2e4c30fc9
Valerie Doan <valeriephuongdoan@gmail.com>
2026-04-14 16:26:06 -0700

    feat(diff-view)(tree): Simple resizable implementation

--- code-review-management/app/(pages)/[username]/[repo_name]/pull/[id]/changes/_components/FileTree/FileTree.tsx
+++ code-review-management/app/(pages)/[username]/[repo_name]/pull/[id]/changes/_components/FileTree/FileTree.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from "react";
 import { FileTreeNode } from "../../_utils/filetree-utils";
 import FileTreeRow from "../FileTreeRow/FileTreeRow";
 import styles from "./FileTree.module.css";
@@ -8,8 +9,47 @@ import styles from "./FileTree.module.css";
  * @param fileTree: Array of `FileTreeNode`s representing the file hierarchy.
  */
 export default function FileTree({ fileTree }: { fileTree: FileTreeNode[] }) {
+  const treeRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const tree = treeRef.current;
+    if (!tree) return;
+
+    const BORDER_SIZE = 4;
+    let mousePosition: number;
+
+    function resize(e: MouseEvent) {
+      if (tree) {
+        const dx = e.x - mousePosition;
+        mousePosition = e.x;
+        tree.style.width =
+          parseInt(getComputedStyle(tree, "").width) + dx + "px";
+      }
+    }
+
+    function onMouseDown(e: MouseEvent) {
+      if (tree && e.offsetX >= tree.offsetWidth - BORDER_SIZE) {
+        mousePosition = e.x;
+        document.addEventListener("mousemove", resize, false);
+      }
+    }
+
+    function onMouseUp() {
+      document.removeEventListener("mousemove", resize, false);
+    }
+
+    tree.addEventListener("mousedown", onMouseDown, false);
+    document.addEventListener("mouseup", onMouseUp, false);
+
+    return () => {
+      tree.removeEventListener("mousedown", onMouseDown, false);
+      document.removeEventListener("mouseup", onMouseUp, false);
+      document.removeEventListener("mousemove", resize, false);
+    };
+  }, []);
+
   return (
-    <div className={styles.tree}>
+    <div className={styles.tree} ref={treeRef}>
       {fileTree.map((node: FileTreeNode) => (
         <FileTreeRow key={node.name} node={node} />
       ))}
