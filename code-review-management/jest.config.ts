9e40365b7e1f8a57944f5f745373fce5cbe15091
Naomi Zhao <naomitan.zhao@gmail.com>
2026-04-13 17:08:32 -0700

    init: Install and configure Jest

---
+++ code-review-management/jest.config.ts
@@ -0,0 +1,21 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+import type { Config } from "jest";
+import nextJest from "next/jest";
+
+const createJestConfig = nextJest({ dir: "./" });
+
+const config: Config = {
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  testEnvironment: "jest-environment-jsdom",
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/$1",
+  },
+};
+
+export default createJestConfig(config);
