69dc37a29e167ca39014652ba083c799e0d2e1b0
Naomi Zhao <naomitan.zhao@gmail.com>
2026-04-14 16:41:03 -0700

    feat(dashboard): Group repo list repos by owner

--- code-review-management/app/(pages)/dashboard/_components/DashboardSidebar/DashboardSidebar.tsx
+++ code-review-management/app/(pages)/dashboard/_components/DashboardSidebar/DashboardSidebar.tsx
@@ -4,9 +4,23 @@ import { Repo } from "@/types/github.types";
 import Checkbox from "@/app/(pages)/_components/Checkbox/Checkbox";
 import { useState } from "react";
 
+function sortReposByOrg(repos: Repo[]) {
+  const mappedRepos = new Map<string, string[]>();
+  for (const repo of repos) {
+    const [owner, name] = repo.full_name.split("/");
+    if (mappedRepos.get(owner)) {
+      mappedRepos.get(owner)!.push(name);
+    } else {
+      mappedRepos.set(owner, [name]);
+    }
+  }
+  return mappedRepos;
+}
+
 export default function DashboardSidebar() {
   const [selectedRepos, setSelectedRepos] = useState<Set<string>>(new Set());
-  const repoList = MOCK_REPOS.data as Repo[];
+  const fullRepoList = MOCK_REPOS.data as Repo[];
+  const mappedRepoList = sortReposByOrg(fullRepoList);
 
   const onCheckboxChange = (name: string, isChecked: boolean) => {
     const newSelectedRepos = new Set(selectedRepos);
@@ -23,18 +37,23 @@ export default function DashboardSidebar() {
     <div className={styles.dashboardSidebar}>
       <div className={styles.sidebarContent}>
         <h4 className={styles.sidebarHeader}>REPOSITORIES</h4>
-        <form className={styles.repoList}>
-          {repoList.map((repo) => (
-            <div key={repo.id} className={styles.repoListEntry}>
-              <Checkbox
-                id={repo.id.toString()}
-                name={repo.name}
-                onChange={onCheckboxChange}
-              />
-              <label htmlFor={repo.id.toString()}>{repo.name}</label>
-            </div>
-          ))}
-        </form>
+        {Array.from(mappedRepoList.keys()).map((owner: string) => (
+          <div className={styles.repoListSection} key={owner}>
+            <h5 className={styles.ownerName}>{owner}</h5>
+            <form className={styles.repoList}>
+              {mappedRepoList.get(owner)?.map((repoName) => (
+                <div key={repoName} className={styles.repoListEntry}>
+                  <Checkbox
+                    id={repoName}
+                    name={repoName}
+                    onChange={onCheckboxChange}
+                  />
+                  <label htmlFor={repoName}>{repoName}</label>
+                </div>
+              ))}
+            </form>
+          </div>
+        ))}
       </div>
       <div className={styles.sideBorder} />
     </div>
