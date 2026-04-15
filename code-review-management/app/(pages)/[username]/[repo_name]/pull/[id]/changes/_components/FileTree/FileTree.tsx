c7688088ce4c20d410ebcea6e77dfbae3fff206f
Valerie Doan <valeriephuongdoan@gmail.com>
2026-04-15 11:13:33 -0700

    refactor(diff-view)(tree): Abstract out resizable logic into separate hook

--- code-review-management/app/(pages)/[username]/[repo_name]/pull/[id]/changes/_components/FileTree/FileTree.tsx
+++ code-review-management/app/(pages)/[username]/[repo_name]/pull/[id]/changes/_components/FileTree/FileTree.tsx
@@ -1,4 +1,5 @@
-import { useEffect, useRef, useState } from "react";
+import { useRef } from "react";
+import { useResizableFileTree } from "../../_hooks/useResizableFileTree";
 import { FileTreeNode } from "../../_utils/filetree-utils";
 import FileTreeRow from "../FileTreeRow/FileTreeRow";
 import styles from "./FileTree.module.css";
@@ -9,77 +10,8 @@ import styles from "./FileTree.module.css";
  * @param fileTree: Array of `FileTreeNode`s representing the file hierarchy.
  */
 export default function FileTree({ fileTree }: { fileTree: FileTreeNode[] }) {
-  const [isResizing, setIsResizing] = useState(false);
-  const [isHovered, setIsHovered] = useState(false);
   const treeRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    const tree = treeRef.current;
-    if (!tree) return;
-
-    const BORDER_SIZE = 4; // Width of the right-side border used for resizing.
-    let startX: number; // Viewport x-coordinate at mouse down (e.clientX).
-    let startTreeWidth: number; // Tree width at mouse down (tree.offsetWidth).
-
-    function applyResize(e: MouseEvent) {
-      if (tree) {
-        const dx = e.clientX - startX; // How far the mouse has moved from where the drag started.
-        tree.style.width = startTreeWidth + dx + "px";
-      }
-    }
-
-    /**
-     * Only start dragging if the click lands within the right-side border of
-     * the file tree.
-     *
-     * e.offsetX: Current mouse position relative to the tree's left-edge.
-     * e.clientX: Current mouse position relative to the entire viewport.
-     * tree.offsetWidth: Current tree's width.
-     */
-    function startResize(e: MouseEvent) {
-      if (tree && e.offsetX >= tree.offsetWidth - BORDER_SIZE) {
-        e.preventDefault();
-
-        startX = e.clientX;
-        startTreeWidth = tree.offsetWidth;
-
-        setIsResizing(true);
-        document.body.style.cursor = "ew-resize";
-        document.addEventListener("mousemove", applyResize, false);
-      }
-    }
-
-    function stopResize() {
-      setIsResizing(false);
-      document.body.style.cursor = "auto";
-      document.removeEventListener("mousemove", applyResize, false);
-    }
-
-    function updateHoverState(e: MouseEvent) {
-      if (tree && e.offsetX >= tree.offsetWidth - BORDER_SIZE) {
-        setIsHovered(true);
-      } else {
-        setIsHovered(false);
-      }
-    }
-
-    function clearHoverState() {
-      if (tree) setIsHovered(false);
-    }
-
-    tree.addEventListener("mousemove", updateHoverState, false);
-    tree.addEventListener("mouseleave", clearHoverState, false);
-    tree.addEventListener("mousedown", startResize, false);
-    document.addEventListener("mouseup", stopResize, false);
-
-    return () => {
-      tree.removeEventListener("mousemove", updateHoverState, false);
-      tree.removeEventListener("mouseleave", clearHoverState, false);
-      tree.removeEventListener("mousedown", startResize, false);
-      document.removeEventListener("mouseup", stopResize, false);
-      document.removeEventListener("mousemove", applyResize, false);
-    };
-  }, []);
+  const { isResizing, isHovered } = useResizableFileTree(treeRef);
 
   return (
     <div
