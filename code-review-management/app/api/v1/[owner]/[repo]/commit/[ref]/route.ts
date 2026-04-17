5eb86cc468db390daed915d590fd865d8c726fde
nithinsenthil <nithin020304@gmail.com>
2026-04-17 02:51:01 -0700

    feat: add commit diffs endpoint

---
+++ code-review-management/app/api/v1/[owner]/[repo]/commit/[ref]/route.ts
@@ -0,0 +1,71 @@
+/*
+/api/v1/{owner}/{repo}/commit/{ref}
+
+*NOT TO BE POLLED*
+*/
+
+import { getCookieName } from "@/app/api/_utils/cookie-utils";
+import { getToken } from "next-auth/jwt";
+import { Octokit, RequestError } from "octokit";
+
+type RouteContext = {
+  params: Promise<{
+    owner: string;
+    repo: string;
+    ref: string;
+  }>;
+};
+
+const secret = process.env.AUTH_SECRET;
+const cookie = getCookieName();
+
+export async function GET(req: Request, context: RouteContext) {
+  const { owner, repo, ref } = await context.params;
+  const token = await getToken({
+    req: req,
+    secret: secret,
+    cookieName: cookie,
+  });
+
+  // Validate token
+  if (token == null || token.accessToken == null || token.githubId == null) {
+    console.log("Unauthorized request at ${new Date()}");
+    return new Response(null, { status: 401 });
+  }
+
+  // Validate required parameters
+  if (!owner || !repo || !ref) {
+    return Response.json(
+      { error: "Missing required parameters" },
+      { status: 400 },
+    );
+  }
+
+  const octokit = new Octokit({ auth: token.accessToken });
+
+  try {
+    const { data: contents } = await octokit.rest.repos.getCommit({
+      owner: owner,
+      repo: repo,
+      ref: ref,
+      mediaType: {
+        format: "diff",
+      },
+    });
+
+    return new Response(JSON.stringify(contents, null, 2), {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (error) {
+    if (error instanceof RequestError && error.status) {
+      // Octokit Http error
+      return new Response(error.message, { status: error.status });
+    } else {
+      // Parsing/other error
+      return new Response("Server error", { status: 500 });
+    }
+  }
+}
