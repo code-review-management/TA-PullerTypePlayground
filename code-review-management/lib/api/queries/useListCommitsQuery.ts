a4f4e589197bec852106c3fb04239d370762650f
Valerie Doan <valeriephuongdoan@gmail.com>
2026-04-17 23:20:27 -0700

    feat(commit-picker): Create TanStack hook to query commit list

---
+++ code-review-management/lib/api/queries/useListCommitsQuery.ts
@@ -0,0 +1,33 @@
+import { useCallback } from "react";
+import { InfiniteData, useInfiniteQuery } from "@tanstack/react-query";
+import { fetcher } from "../utils/fetcher";
+import { CommitV2 } from "@/types/github.types.wrapper";
+
+/**
+ * Fetches the commits in a GitHub pull request. Supports pagination.
+ *
+ * @param owner: Owner of the repository.
+ * @param repo: Name of the repository.
+ * @param pullNumber: Pull request number.
+ * @param branch: Feature branch of the pull request.
+ * @returns: TanStack query result containing the list of pull request commits.
+ */
+export function useListFilesQuery(
+  owner: string,
+  repo: string,
+  pullNumber: string,
+  branch: string,
+) {
+  return useInfiniteQuery({
+    queryKey: ["list-commits", owner, repo, pullNumber],
+    queryFn: async ({ pageParam }): Promise<CommitV2> =>
+      fetcher(
+        `/api/v2/${owner}/${repo}/pulls/${pullNumber}/commits?page=${pageParam}?branch=${branch}`,
+      ),
+    initialPageParam: 1,
+    getNextPageParam: (lastPage) => lastPage.next,
+    select: useCallback((data: InfiniteData<CommitV2, number>) => {
+      data.pages.flatMap((page) => page.data);
+    }, []),
+  });
+}
