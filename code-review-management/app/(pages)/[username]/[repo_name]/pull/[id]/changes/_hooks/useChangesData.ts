fc14085804dcb335964f4c2d818cc975acacc129
Valerie Doan <valeriephuongdoan@gmail.com>
2026-04-19 15:18:17 -0700

    feat(commit-picker): Fetch commit files if SHA is in search params

--- code-review-management/app/(pages)/[username]/[repo_name]/pull/[id]/changes/_hooks/useChangesData.ts
+++ code-review-management/app/(pages)/[username]/[repo_name]/pull/[id]/changes/_hooks/useChangesData.ts
@@ -1,11 +1,13 @@
+import { useParams, useSearchParams } from "next/navigation";
 import { PullParams } from "@/types/routing.types";
 import { useAutoFetchAllPages } from "@/lib/api/hooks/useAutoFetchAllPages";
+import { useCommitQuery } from "@/lib/api/queries/useCommitQuery";
 import { useListFilesQuery } from "@/lib/api/queries/useListFilesQuery";
-import { useParams } from "next/navigation";
-import { usePublishedThreads } from "./usePublishedThreads";
 import { usePullQuery } from "@/lib/api/queries/usePullQuery";
+import { usePublishedThreads } from "./usePublishedThreads";
 
 export function useChangesData() {
+  const sha = useSearchParams().get("sha");
   const { username, repo_name, id } = useParams<PullParams>();
 
   const {
@@ -30,11 +32,31 @@ export function useChangesData() {
   } = useListFilesQuery(username, repo_name, id);
   useAutoFetchAllPages(hasNextFilesPage, isFilesFetching, fetchNextFilesPage);
 
+  const {
+    data: commitFiles,
+    hasNextPage: hasNextCommitFilesPage,
+    fetchNextPage: fetchNextCommitFilesPage,
+    isFetching: isCommitFilesFetching,
+    isPending: isCommitFilesPending,
+    isError: isCommitFilesError,
+  } = useCommitQuery(username, repo_name, sha ?? "", sha !== null);
+  useAutoFetchAllPages(
+    hasNextCommitFilesPage,
+    isCommitFilesFetching,
+    fetchNextCommitFilesPage,
+  );
+
   return {
     pull,
-    files,
+    files: sha ? commitFiles?.files : files,
     publishedThreads,
-    isPending: isPullPending || isFilesPending || isPublishedThreadsPending,
-    isError: isPullError || isFilesError || isPublishedThreadsError,
+    isPending:
+      isPullPending ||
+      isPublishedThreadsPending ||
+      (sha ? isCommitFilesPending : isFilesPending),
+    isError:
+      isPullError ||
+      isPublishedThreadsError ||
+      (sha ? isCommitFilesError : isFilesError),
   };
 }
