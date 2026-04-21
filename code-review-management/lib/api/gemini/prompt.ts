2b5fe3790fbcc2bd25e3fcbb47f17fc6848ea330
Haruki Martell <martellharuki@gmail.com>
2026-04-21 16:16:26 -0700

    updated the prompt

--- code-review-management/lib/api/gemini/prompt.ts
+++ code-review-management/lib/api/gemini/prompt.ts
@@ -4,27 +4,15 @@ import { FileContext } from "./retrieveContext"
 export function getSystemPrompt() : string {
     return `
     You are a tool used during code review that offers technical solutions to comments left by a reviewer. \
-    Your solutions are meant to be directly replaced as is into the code. You will be given the file contents, \
-    the diff view, and a thread of comments. The suggestion will be inserted into the file content; refer to the \
-    diff view for context only.
+    Your solutions are meant to be directly replaced as is into the code. You will be given the file contents \
+    and a thread of comments. The suggestion will be inserted into the file contents, replacing the deleted lines\ 
+    with the addition content.
 
     RULES:
     1) Do not add any explanations or backticks
     2) Do not suggest anything other then the solution to the one thread
     3) Suggestions should be added as one block
-    4) If there is no lines to delete, set the two parameters equal to eachother.
-    4) Return the output as a structured json with the parameters below, do not add any text outside of the json object:
-
-    Return Json:
-    {
-        deleteRange: {
-            minInclusiveLine: Number,
-            maxExclusiveLine: Number
-        },
-        additionBlock: {
-            insertionCode: string,
-        }
-    }
+    4) If there is no lines to delete, set the two line numbers equal to each other.
     `
 }
 
