5b0a1654130e095ea583a85f9c6128d342517734
Naomi Zhao <naomitan.zhao@gmail.com>
2026-04-22 13:18:25 -0700

    feat(dashboard): Auto fetch all pages for pulls

--- code-review-management/app/(pages)/dashboard/page.tsx
+++ code-review-management/app/(pages)/dashboard/page.tsx
@@ -4,15 +4,18 @@ import IconTooltip from "../_components/IconTooltip/IconTooltip";
 import DashboardGrid from "./_components/DashboardGrid/DashboardGrid";
 import styles from "./page.module.css";
 import LoadingSpinner from "../_components/LoadingSpinner/LoadingSpinner";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import DashboardSearchBar from "./_components/DashboardSearch/DashboardSearchBar";
 import DashboardSidebar from "./_components/DashboardSidebar/DashboardSidebar";
 import { sortPullsByUpdated } from "./_utils/pulls-utils";
 import { useLocalStorage } from "usehooks-ts";
+import { useAutoFetchAllPages } from "@/lib/api/hooks/useAutoFetchAllPages";
 
 export default function Dashboard() {
   const { data, fetchNextPage, hasNextPage, isFetching, isPending } =
     usePullsQuery();
+  useAutoFetchAllPages(hasNextPage, isFetching, fetchNextPage);
+
   const [searchString, setSearchString] = useState("");
   const [appliedSearchString, setAppliedSearchString] = useState("");
   const [selectedRepos, setSelectedRepos] = useLocalStorage<string[]>(
@@ -23,25 +26,6 @@ export default function Dashboard() {
 
   const pulls = data?.pages.flatMap((page) => page.data) ?? [];
   const sortedPulls = sortPullsByUpdated(pulls);
-
-  // Auto fetch all remaining pulls if any filter is applied
-  // TODO: Limit by # of returned pulls
-  useEffect(() => {
-    if (
-      (appliedSearchString.length !== 0 || selectedRepos.length !== 0) &&
-      hasNextPage &&
-      !isFetching
-    ) {
-      console.log("fetch");
-      fetchNextPage();
-    }
-  }, [
-    appliedSearchString,
-    selectedRepos,
-    hasNextPage,
-    isFetching,
-    fetchNextPage,
-  ]);
 
   return (
     <div className={styles.page}>
