b7ee15d34770ed0ea452785aa684b7da8062022e
Valerie Doan <valeriephuongdoan@gmail.com>
2026-04-21 19:04:12 -0700

    feat(commit-picker): Add back to all changes button

--- code-review-management/app/(pages)/[username]/[repo_name]/pull/[id]/changes/_components/CommitViewBanner/CommitViewBanner.test.tsx
+++ code-review-management/app/(pages)/[username]/[repo_name]/pull/[id]/changes/_components/CommitViewBanner/CommitViewBanner.test.tsx
@@ -1,12 +1,48 @@
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import CommitViewBanner from "./CommitViewBanner";
 
+const mockRouterPush = jest.fn();
+
+jest.mock("next/navigation", () => ({
+  useParams: () => ({
+    username: "owner",
+    repo_name: "repo",
+    id: "1",
+  }),
+  useRouter: () => ({
+    push: mockRouterPush,
+  }),
+}));
+
 describe("CommitViewBanner", () => {
+  const mockSha = "ab102f9301df14";
+
+  beforeEach(() => {
+    jest.clearAllMocks(); // Clear `mockRouterPush` usage data.
+  });
+
   it("renders message with shortened SHA", () => {
-    render(<CommitViewBanner sha={"ab102f9301df14"} />);
+    render(<CommitViewBanner sha={mockSha} />);
     expect(screen.getByTestId("commit-view-banner")).toHaveTextContent(
       "Commenting is disabled while viewing commit ab102f9",
     );
+  });
+
+  it("renders back to all changes button", () => {
+    render(<CommitViewBanner sha={mockSha} />);
+    expect(
+      screen.getByRole("button", { name: "Back to all changes" }),
+    ).toBeInTheDocument();
+  });
+
+  it("routes the user to the default changes page on button click", async () => {
+    const user = userEvent.setup();
+    render(<CommitViewBanner sha={mockSha} />);
+    await user.click(
+      screen.getByRole("button", { name: "Back to all changes" }),
+    );
+    expect(mockRouterPush).toHaveBeenCalledWith("/owner/repo/pull/1/changes");
   });
 });
