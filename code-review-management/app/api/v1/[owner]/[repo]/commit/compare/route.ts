519d3e9a247e862f23d283ed1533bb0cb8135ef1
Nithin Senthil <nithin020304@gmail.com>
2026-04-23 18:32:47 -0700

    chore: update variable names for clarity

--- code-review-management/app/api/v1/[owner]/[repo]/commit/compare/route.ts
+++ code-review-management/app/api/v1/[owner]/[repo]/commit/compare/route.ts
@@ -55,7 +55,7 @@ export async function GET(req: Request, context: RouteContext) {
   const octokit = new Octokit({ auth: token.accessToken });
 
   try {
-    const { data: tempContents } =
+    const { data: contents } =
       await octokit.rest.repos.compareCommitsWithBasehead({
         owner: owner,
         repo: repo,
@@ -64,11 +64,11 @@ export async function GET(req: Request, context: RouteContext) {
 
     // Filter temp response
     let filteredResponse: CompareCommits =
-      CompareCommitsSchema.parse(tempContents);
+      CompareCommitsSchema.parse(contents);
 
     // If the base is different from the merge base commit, recompute
     if (base != filteredResponse.merge_base_commit.sha) {
-      const { data: contents } =
+      const { data: newContents } =
         await octokit.rest.repos.compareCommitsWithBasehead({
           owner: owner,
           repo: repo,
@@ -76,7 +76,7 @@ export async function GET(req: Request, context: RouteContext) {
         });
 
       // Filter response
-      filteredResponse = CompareCommitsSchema.parse(contents);
+      filteredResponse = CompareCommitsSchema.parse(newContents);
     }
 
     return new Response(JSON.stringify(filteredResponse, null, 2), {
