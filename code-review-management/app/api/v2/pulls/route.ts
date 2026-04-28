6b28dfe0c80ce8a36cac5d30c32432bad7b96a29
Nithin Senthil <nithin020304@gmail.com>
2026-04-28 09:50:32 -0700

    feat: add customizable query string

--- code-review-management/app/api/v2/pulls/route.ts
+++ code-review-management/app/api/v2/pulls/route.ts
@@ -14,6 +14,18 @@ import parse from "parse-link-header";
 const secret = process.env.AUTH_SECRET;
 const cookie = getCookieName();
 
+const qParams = {
+  open: ["state:open", "state:closed"],
+  draft: ["draft:true", "draft:false"],
+  merged: ["is:merged", "is:unmerged"],
+  needs_review: ["user-review-requested:@me"],
+  ready_for_review: ["draft:false review:required"],
+  approved: ["review:approved"],
+  opened: ["author:@me"],
+  assigned: ["assignee:@me"],
+  reviewed: ["reviewed-by:@me"],
+} as const;
+
 export async function GET(req: Request) {
   const token = await getToken({
     req: req,
@@ -30,9 +42,6 @@ export async function GET(req: Request) {
   // Get query parameters
   const { searchParams: params } = new URL(req.url);
   const page = Number(params.get("page"));
-  const open = params.get("open");
-  const draft = params.get("draft");
-  const merged = params.get("merged");
 
   // Validate parameters
   if (isNaN(page) || page < 1) {
@@ -44,15 +53,19 @@ export async function GET(req: Request) {
 
   const octokit = new Octokit({ auth: token.accessToken });
 
-  try {
-    let query = "is:pr involves:@me ";
-    query +=
-      open != null ? (open == "true" ? "state:open " : "state:closed ") : "";
-    query +=
-      draft != null ? (draft == "true" ? "draft:true " : "draft:false ") : "";
-    query +=
-      merged != null ? (merged == "true" ? "is:merged " : "is:unmerged ") : "";
+  // Assemble query string
+  let query = "is:pr involves:@me ";
+  Object.entries(qParams).forEach(([key, value]) => {
+    const param = params.get(key);
+    const index = Number(param);
+    if (param == null || isNaN(index) || index < 0 || index >= value.length)
+      return;
 
+    query += value[index] + " ";
+  });
+  query = query.slice(0, -1);
+
+  try {
     const data = await octokit.request("GET /search/issues", {
       q: query,
       page: page,
