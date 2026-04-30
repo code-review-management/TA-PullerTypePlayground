7f6f6cf71b440accf1726583e8b82ac6b1510b87
Valerie Doan <valeriephuongdoan@gmail.com>
2026-04-30 13:38:18 -0700

    feat: Style login page

--- code-review-management/app/(pages)/(auth)/sign-in/page.tsx
+++ code-review-management/app/(pages)/(auth)/sign-in/page.tsx
@@ -1,5 +1,8 @@
 import { signIn } from "@/lib/auth";
+import Image from "next/image";
+import GitHubIcon from "@/public/icons/github.svg";
 import styles from "./page.module.css";
+import Link from "next/link";
 
 /**
  * Docs:
@@ -9,16 +12,30 @@ import styles from "./page.module.css";
 export default function SignIn() {
   return (
     <div className={styles.page}>
-      <form
-        action={async () => {
-          "use server";
-          await signIn("github", { redirectTo: "/dashboard" });
-        }}
-      >
-        <button type="submit" className={styles.signInButton}>
-          Sign in with GitHub
-        </button>
-      </form>
+      <div className={styles.container}>
+        <div className={styles.heading}>
+          <h1>PullerType</h1>
+          <p>Welcome back to PullerType</p>
+        </div>
+        <form
+          action={async () => {
+            "use server";
+            await signIn("github", { redirectTo: "/dashboard" });
+          }}
+        >
+          <button type="submit" className={styles.signInButton}>
+            <Image src={GitHubIcon} alt="GitHub" />
+            Sign in with GitHub
+          </button>
+        </form>
+        <p className={styles.notice}>
+          By signing up, you acknowledge that you read and agree to our{" "}
+          <Link className={styles.link} href="">
+            Privacy Policy
+          </Link>
+          .
+        </p>
+      </div>
     </div>
   );
 }
