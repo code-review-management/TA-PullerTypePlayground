10e87a36be442dc93818d3d810693da0ebfd4ea7
Valerie Doan <valeriephuongdoan@gmail.com>
2026-05-01 18:06:49 -0700

    feat(navbar): Begin adding icons

--- code-review-management/app/(pages)/_components/Navbar/Navbar.tsx
+++ code-review-management/app/(pages)/_components/Navbar/Navbar.tsx
@@ -1,5 +1,45 @@
+"use client";
+
+import { signOut, useSession } from "next-auth/react";
+import Image from "next/image";
+import Link from "next/link";
+import IconTooltip from "../IconTooltip/IconTooltip";
+import InboxIcon from "@/public/icons/navbar/inbox.svg";
+import SignOutIcon from "@/public/icons/navbar/sign_out.svg";
+import UserIcon from "../UserIcon/UserIcon";
 import styles from "./Navbar.module.css";
 
 export default function Navbar() {
-  return <div className={styles.navbar}>Woot</div>;
+  const { data: session } = useSession();
+
+  return (
+    <div>
+      <div className={styles.navbar}>
+        <div className={styles.group}>
+          <Link
+            href="/dashboard"
+            data-tooltip-id={"tooltip-navbar"}
+            data-tooltip-content={"Dashboard"}
+            data-tooltip-place="right"
+            data-tooltip-delay-show={100}
+          >
+            <Image src={InboxIcon} alt="Inbox" />
+          </Link>
+        </div>
+        <div className={styles.group}>
+          <button onClick={() => signOut()} className={styles.signOutButton}>
+            <Image src={SignOutIcon} alt="Sign out" />
+          </button>
+          {session?.user.image && (
+            <UserIcon
+              username={session.user.githubLogin}
+              avatarUrl={session.user.image}
+              size={24}
+            />
+          )}
+        </div>
+      </div>
+      <IconTooltip id="tooltip-navbar" positionStrategy="fixed" />
+    </div>
+  );
 }
