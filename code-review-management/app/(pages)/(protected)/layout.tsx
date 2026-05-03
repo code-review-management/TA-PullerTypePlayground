8b809b4b792418be78883a74d556eefbbcca1e94
Valerie Doan <valeriephuongdoan@gmail.com>
2026-05-02 23:39:03 -0700

    refactor(navbar): Rename layout for shared route group

--- code-review-management/app/(pages)/(protected)/layout.tsx
+++ code-review-management/app/(pages)/(protected)/layout.tsx
@@ -2,7 +2,7 @@ import Navbar from "../_components/Navbar/Navbar";
 import ProtectedLayout from "../_components/ProtectedLayout/ProtectedLayout";
 import styles from "./layout.module.css";
 
-export default function AuthenticatedLayout({
+export default function MainLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
