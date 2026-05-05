3dec458a5dd8ff537b9dc111c497f9ce510d33f8
Naomi Zhao <naomitan.zhao@gmail.com>
2026-05-04 19:09:47 -0700

    chore(dashboard): Remove change from sortPullsByUpdated comment

--- code-review-management/app/(pages)/(protected)/dashboard/_utils/pulls-utils.ts
+++ code-review-management/app/(pages)/(protected)/dashboard/_utils/pulls-utils.ts
@@ -3,9 +3,8 @@ import { PullRequest } from "@/types/github.types";
 /**
  * Sorts an array of PullRequests by update time (updated_at),
  * with the most recent pull requests first.
- * 
- * @param pulls Array of PullRequest objects.
- * @returns Sorted array of `PullRequest` objects.
+ * @param pulls Array of PullRequest objects
+ * @returns Sorted array of PullRequest objects
  */
 export function sortPullsByUpdated(pulls: PullRequest[]) {
   return pulls.toSorted(
