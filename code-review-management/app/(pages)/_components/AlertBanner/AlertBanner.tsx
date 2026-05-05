f1e68ceae9d47f551f24a87873cb93cfa7d289cd
Valerie Doan <valeriephuongdoan@gmail.com>
2026-05-04 17:40:11 -0700

    style(diff-view)(panel): Add alert icon in banner

--- code-review-management/app/(pages)/_components/AlertBanner/AlertBanner.tsx
+++ code-review-management/app/(pages)/_components/AlertBanner/AlertBanner.tsx
@@ -1,6 +1,17 @@
 import { ReactNode } from "react";
+import AlertFillIcon from "@/public/icons/alert_fill.svg";
 import styles from "./AlertBanner.module.css";
+import Image from "next/image";
 
+/**
+ * Banner for displaying alert messages.
+ * @param children: Content to display in the banner.
+ */
 export default function AlertBanner({ children }: { children: ReactNode }) {
-  return <div className={styles.banner}>{children}</div>;
+  return (
+    <div className={styles.banner}>
+      <Image src={AlertFillIcon} alt="Alert" />
+      {children}
+    </div>
+  );
 }
