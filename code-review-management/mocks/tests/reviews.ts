77e99f32c15df741582ebf533c0c828cb56a42e9
Naomi Zhao <naomitan.zhao@gmail.com>
2026-05-06 15:19:02 -0700

    tests: Remove unused mock reviews

--- code-review-management/mocks/tests/reviews.ts
+++ code-review-management/mocks/tests/reviews.ts
@@ -1,27 +1,4 @@
 import { Review, User } from "@/types/github.types";
-import { getExampleUser1, getExampleUser2 } from "./users";
-
-export function getExampleReview1(): Review {
-  return {
-    id: 1,
-    user: getExampleUser1(),
-    body: "",
-    html_url: "",
-    state: "APPROVED",
-    author_association: "COLLABORATOR",
-  };
-}
-
-export function getExampleReview2(): Review {
-  return {
-    id: 1,
-    user: getExampleUser2(),
-    body: "",
-    html_url: "",
-    state: "CHANGES_REQUESTED",
-    author_association: "COLLABORATOR",
-  };
-}
 
 export function createExampleReview(
   user: User,
