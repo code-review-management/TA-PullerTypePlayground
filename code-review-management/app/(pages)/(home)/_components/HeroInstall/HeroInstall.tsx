9a07d184d10aa81d16e8b888308e936d86c2a295
Naomi Zhao <naomitan.zhao@gmail.com>
2026-05-08 15:38:49 -0700

    feat(home): Add sign in and install capabilities to hero buttons

--- code-review-management/app/(pages)/(home)/_components/HeroInstall/HeroInstall.tsx
+++ code-review-management/app/(pages)/(home)/_components/HeroInstall/HeroInstall.tsx
@@ -1,12 +1,20 @@
 import Image from "next/image";
 import styles from "./HeroInstall.module.css";
 import GitHubIcon from "@/public/icons/github.svg";
+import { EXTERNAL_LINKS } from "@/lib/links";
 
 export default function HeroInstall() {
   return (
-    <button className={styles.btn}>
-      <Image src={GitHubIcon} alt="GitHub" height={30} width={30} />
-      Install GitHub App
-    </button>
+    <a
+      href={EXTERNAL_LINKS.GITHUB_APP_INSTALLATION}
+      target="_blank"
+      rel="noopener noreferrer"
+      className={styles.btnLink}
+    >
+      <button className={styles.btn}>
+        <Image src={GitHubIcon} alt="GitHub" height={30} width={30} />
+        Install GitHub App
+      </button>
+    </a>
   );
 }
