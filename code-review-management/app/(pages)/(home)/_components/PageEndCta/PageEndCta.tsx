d3bcb907638dcb96464494b1b3180582143d4129
Naomi Zhao <naomitan.zhao@gmail.com>
2026-05-08 15:25:02 -0700

    feat(home): Add PageEndCta content

--- code-review-management/app/(pages)/(home)/_components/PageEndCta/PageEndCta.tsx
+++ code-review-management/app/(pages)/(home)/_components/PageEndCta/PageEndCta.tsx
@@ -1,9 +1,15 @@
+import HeroInstall from "../HeroInstall/HeroInstall";
+import HeroSignIn from "../HeroSignIn/HeroSignIn";
 import styles from "./PageEndCta.module.css";
 
 export default function PageEndCta() {
   return (
     <div className={styles.pageEndCta}>
       <h2>Try it now!</h2>
+      <div className={styles.buttonSection}>
+        <HeroInstall />
+        <HeroSignIn />
+      </div>
     </div>
   );
 }
