66b21d1c90764c7bf5da6c298751fa91890979f8
Valerie Doan <valeriephuongdoan@gmail.com>
2026-05-11 01:47:40 -0700

    chore(auth): Comment out debouncer for unauthenticated status check

--- code-review-management/app/(pages)/_components/ProtectedLayout/ProtectedLayout.tsx
+++ code-review-management/app/(pages)/_components/ProtectedLayout/ProtectedLayout.tsx
@@ -19,13 +19,13 @@ export default function ProtectedLayout({ children }: { children: ReactNode }) {
     if (session?.error === "AccessTokenExpired") {
       signIn();
     }
-
-    // If status is unauthenticated for 2 second, then sign-out. Used by other
-    // open tabs when a sign-out is initiated in one tab.
-    if (status === "unauthenticated") {
-      const signInTimeout = setTimeout(() => {
-        signIn();
-      }, 2000);
+    // If another tab signs out, then this tab will see "unauthenticated". Wait
+    // for two seconds before directing the user to the sign-in page. The
+    // debouncer is just in case there is a temporary "unauthenticated" state
+    // that is quickly resolved to "authenticated" (e.g., solving runtime
+    // errors).
+    else if (status === "unauthenticated") {
+      const signInTimeout = setTimeout(signIn, 2000);
       return () => clearTimeout(signInTimeout);
     }
   }, [session?.error, status]);
