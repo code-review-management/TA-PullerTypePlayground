189d8afed37482068a0a5b7706d410ef8ecbf27d
Valerie Doan <valeriephuongdoan@gmail.com>
2026-05-11 01:50:53 -0700

    feat(auth): Update debouncer from 2s to 3s for unauthenticated check

--- code-review-management/app/(pages)/_components/ProtectedLayout/ProtectedLayout.tsx
+++ code-review-management/app/(pages)/_components/ProtectedLayout/ProtectedLayout.tsx
@@ -16,16 +16,17 @@ export default function ProtectedLayout({ children }: { children: ReactNode }) {
   const { data: session, status } = useSession();
 
   useEffect(() => {
+    console.log(status);
     if (session?.error === "AccessTokenExpired") {
       signIn();
     }
     // If another tab signs out, then this tab will see "unauthenticated". Wait
-    // for two seconds before directing the user to the sign-in page. The
+    // for three seconds before directing the user to the sign-in page. The
     // debouncer is just in case there is a temporary "unauthenticated" state
     // that is quickly resolved to "authenticated" (e.g., solving runtime
     // errors).
     else if (status === "unauthenticated") {
-      const signInTimeout = setTimeout(signIn, 2000);
+      const signInTimeout = setTimeout(signIn, 3000);
       return () => clearTimeout(signInTimeout);
     }
   }, [session?.error, status]);
