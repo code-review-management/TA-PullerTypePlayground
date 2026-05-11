40808ed29630dc8ed0438e2f8f250003ffa4f281
Valerie Doan <valeriephuongdoan@gmail.com>
2026-05-10 19:47:11 -0700

    chore(perms): Clarify usePermissionChecks comment

--- code-review-management/app/(pages)/(protected)/[username]/[repo_name]/pull/[id]/_hooks/usePermissionChecks.ts
+++ code-review-management/app/(pages)/(protected)/[username]/[repo_name]/pull/[id]/_hooks/usePermissionChecks.ts
@@ -6,14 +6,14 @@ type AccessType = "implicit-read" | "explicit-read" | "write";
  * API error message when GitHub App is not installed: "Resource not accessible
  * by integration" (403 status)
  *
- * API error message when GitHub App is installed but user does not have push
- * access: "Must have push access to view collaborator permission" (403 status).
+ * API error message when user does not have push access: "Must have push access
+ * to view collaborator permission" (403 status).
  * - Note: It seems like this API error message only appears if requesting from
  *   outside the GitHub App. When requesting through the GitHub App in this
  *   scenario, we get the "Resource not accessible by integration" message
  *   instead. I will still keep the logic for this message check, but as far
  *   as I know, I do not know a scenario where it is being applied. This means
- *   the access type "explicit-read" will likely be unused.
+ *   the access type "explicit-read" is likely unassigned.
  */
 export function usePermissionChecks() {
   const { data, error, isPending } = usePermissionContext();
