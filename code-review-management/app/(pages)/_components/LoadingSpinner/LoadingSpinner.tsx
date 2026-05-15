dfd85a3cc01f7dcc11fd9424da6ce106ecdfab67
Valerie Doan <valeriephuongdoan@gmail.com>
2026-05-15 02:29:39 -0700

    chore: Updates comments for LoadingSpinner props

--- code-review-management/app/(pages)/_components/LoadingSpinner/LoadingSpinner.tsx
+++ code-review-management/app/(pages)/_components/LoadingSpinner/LoadingSpinner.tsx
@@ -5,8 +5,8 @@ import styles from "./LoadingSpinner.module.css";
  * Renders a loading spinner.
  *
  * @param size: Height and width of the spinner in px units. Defaults to 20px.
- * @param centered: Whether the spinner should be centered within 100% of its
- *                  parent's width.
+ * @param centered: Whether the spinner should be centered within its parent
+ *                  container.
  * @param forPageLevel: Whether the spinner indicates loading for a page-level
  *                      component.
  */
