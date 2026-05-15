1a289bb4ffdb0f10888becf4a47a567dbbd67229
Valerie Doan <valeriephuongdoan@gmail.com>
2026-05-15 01:47:47 -0700

    feat: Use loading spinner if page-level is pending

--- code-review-management/app/(pages)/_components/LoadingSpinner/LoadingSpinner.tsx
+++ code-review-management/app/(pages)/_components/LoadingSpinner/LoadingSpinner.tsx
@@ -1,12 +1,29 @@
 import { RotatingLines } from "react-loader-spinner";
+import styles from "./LoadingSpinner.module.css";
 
 /**
  * Renders a loading spinner.
  *
  * @param size: Height and width of the spinner in px units. Defaults to 20px.
+ * @param centered: Whether the spinner should be centered within 100% of its
+ *                  parent's width.
+ * @param forPageLevel: Whether the spinner is to indicate loading for a
+ *                      page-level component.
  */
-export default function LoadingSpinner({ size = 20 }: { size?: number }) {
+export default function LoadingSpinner({
+  size = 20,
+  centered,
+  forPageLevel,
+}: {
+  size?: number;
+  centered?: boolean;
+  forPageLevel?: boolean;
+}) {
   return (
-    <RotatingLines height={size} width={size} color="var(--color-disabled)" />
+    <div
+      className={`${forPageLevel ? styles.pageMargin : ""} ${centered ? styles.centered : ""}`}
+    >
+      <RotatingLines height={size} width={size} color="var(--color-disabled)" />
+    </div>
   );
 }
