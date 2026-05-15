48ad151287b7fbc822251886cdf13356256b9a6f
Valerie Doan <valeriephuongdoan@gmail.com>
2026-05-15 02:09:51 -0700

    chore: Update comment for LoadingSpinner param

--- code-review-management/app/(pages)/_components/LoadingSpinner/LoadingSpinner.tsx
+++ code-review-management/app/(pages)/_components/LoadingSpinner/LoadingSpinner.tsx
@@ -7,8 +7,8 @@ import styles from "./LoadingSpinner.module.css";
  * @param size: Height and width of the spinner in px units. Defaults to 20px.
  * @param centered: Whether the spinner should be centered within 100% of its
  *                  parent's width.
- * @param forPageLevel: Whether the spinner is to indicate loading for a
- *                      page-level component.
+ * @param forPageLevel: Whether the spinner indicates loading for a page-level
+ *                      component.
  */
 export default function LoadingSpinner({
   size = 20,
