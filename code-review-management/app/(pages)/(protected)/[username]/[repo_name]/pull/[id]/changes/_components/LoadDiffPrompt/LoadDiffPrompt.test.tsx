e61f8cf5966b339d9def228faee3b3448b8acb30
Valerie Doan <valeriephuongdoan@gmail.com>
2026-05-16 17:48:23 -0700

    tests(diff-view): Add unit tests for LoadDiffPrompt

---
+++ code-review-management/app/(pages)/(protected)/[username]/[repo_name]/pull/[id]/changes/_components/LoadDiffPrompt/LoadDiffPrompt.test.tsx
@@ -0,0 +1,62 @@
+import "@testing-library/jest-dom";
+import { ComponentProps } from "react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import LoadDiffPrompt, { LoadDiffReason } from "./LoadDiffPrompt";
+
+describe("LoadDiffPrompt", () => {
+  const mockSetIsDiffLoaded = jest.fn();
+  const defaultProps: ComponentProps<typeof LoadDiffPrompt> = {
+    setIsDiffLoaded: mockSetIsDiffLoaded,
+    reason: null,
+  };
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("renders the load diff button", () => {
+    render(<LoadDiffPrompt {...defaultProps} />);
+    expect(
+      screen.getByRole("button", { name: "Load diff" }),
+    ).toBeInTheDocument();
+  });
+
+  it("calls setIsDIffLoaded on button click", async () => {
+    const user = userEvent.setup();
+    render(<LoadDiffPrompt {...defaultProps} />);
+    await user.click(screen.getByRole("button", { name: "Load diff" }));
+    expect(mockSetIsDiffLoaded).toHaveBeenCalled();
+  });
+
+  it("passes true to setIsDIffLoaded on button click", async () => {
+    const user = userEvent.setup();
+    render(<LoadDiffPrompt {...defaultProps} />);
+    await user.click(screen.getByRole("button", { name: "Load diff" }));
+    expect(mockSetIsDiffLoaded).toHaveBeenCalledWith(true);
+  });
+
+  it("does not render the description if reason is null", () => {
+    render(<LoadDiffPrompt {...defaultProps} reason={null} />);
+    expect(screen.queryByTestId("description")).not.toBeInTheDocument();
+  });
+
+  it("render the correct description if reason is 'removed'", () => {
+    render(<LoadDiffPrompt {...defaultProps} reason={"removed"} />);
+    expect(screen.getByTestId("description")).toHaveTextContent(
+      "This file was removed.",
+    );
+  });
+
+  it.each(["size-limit", "line-limit"])(
+    "renders the correct description if reason is '%s'",
+    (reason) => {
+      render(
+        <LoadDiffPrompt {...defaultProps} reason={reason as LoadDiffReason} />,
+      );
+      expect(screen.getByTestId("description")).toHaveTextContent(
+        "Large diffs are not rendered by default.",
+      );
+    },
+  );
+});
