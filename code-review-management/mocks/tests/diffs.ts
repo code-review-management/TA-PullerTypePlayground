6f9488dcf612077e5fd87711224d64248b70a947
Nithin Senthil <66451044+nithinsenthil@users.noreply.github.com>
2026-06-03 16:39:49 -0700

    Committing suggestion

--- code-review-management/mocks/tests/diffs.ts
+++ code-review-management/mocks/tests/diffs.ts
@@ -1,8 +1,10 @@
 import { ActiveHighlight } from "@/app/(pages)/(protected)/[username]/[repo_name]/pull/[id]/changes/_hooks/useHighlight";
 import { ChangeData } from "react-diff-view";
-
-export function getDefaultInsertChangeData(): ChangeData {
-  return {
+/**
+ * Returns a default object representing an inserted line in a diff.
+ * @returns {ChangeData} A default insert change data object.
+ * Modification
+ */  return {
     type: "insert",
     content: "insert-content",
     lineNumber: 1,
